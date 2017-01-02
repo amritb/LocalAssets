fa3ebc657ef774b5a576780f0b8d1ebfaf2467d9
Berend de Jong <amanuo@gmail.com>
2017-01-03 00:30:14 +0700

    Support for background images defined in style attributes

--- content_script.js
+++ content_script.js
@@ -3,12 +3,20 @@ chrome.storage.sync.get('value', function(items){
   for (var key in items.value) {
     var obj = items.value[key];
     for (var prop in obj) {
-      console.log(prop + '= ' + obj[prop]);
+      //console.log(prop + '= ' + obj[prop]);
       if(host === obj.d) {
+
+        // <img src="">
         jQuery('img').each(function(){
           var url = jQuery(this).attr('src').replace(obj.f, obj.r);
           jQuery(this).attr('src', url);
         });
+
+        // <* style="">
+        jQuery('[style]').each(function(){
+          jQuery(this).attr('style', jQuery(this).attr('style').replace(obj.f, obj.r) );
+        });
+
       }
     }
   };
